a1f56e525652e4cfec6a9b44a300b5ea889da13f
TreydenBrotherton <116790868+TreydenBrotherton@users.noreply.github.com>
2023-05-22 13:02:40 +0800

    Start of GUI

--- DroneService/DroneService/MainWindow.xaml.cs
+++ DroneService/DroneService/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace DroneService
         {
             InitializeComponent();
         }
+
+        // --- Methods --- //
+
+        // AddNewItem - Adds a new service item (Client Name, Drone Model, Service Problem & Service Cost)
+
+        // GetServicePriority - Returns a value of the selected radio button (Regular or Express) (Must be called inside AddNewItem before adding)
+
+        // RegularServiceDisplay - Displays the Regular Service queue to a list view
+
+        // ExpressServiceDisplay - Displays the Express Service queue to a list view
+
+        // IncrementServiceTag - Increments the service tag control (Must be called inside AddNewItem before adding)
+
+        // ClearTextBoxes - Clears all text boxes (Used after a service item is added)
+
+        // --- Buttons --- //
+
+        // DeleteRegularItem - Deletes item from the Regular listview and dequeues the regular service queue (Dequeued items must be added to a finished list)
+
+        // DeleteExpressItem - Deletes item from the Express listview and dequeues the Express service queue (Dequeued items must be added to a finished list)
+
+        // DoubleClickRemove - Double click to remove item from finished listbox and remove the same item from the finished list
+
+        // MouseClickRegularItem - Display client name & service problem in related text boxes for Regular items
+
+        // MouseClickExpressItem - Display client name & service problem in related text boxes for Express items
+
+
+
+
     }
 }
